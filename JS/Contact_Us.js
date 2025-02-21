97c0f3bb05b81a9cb8131fe9abfbe465a6884a1d
Rishabh <rishabhshah2005@gmail.com>
2025-02-21 17:34:15 +0530

    added email functions

--- JS/Contact_Us.js
+++ JS/Contact_Us.js
@@ -14,29 +14,31 @@ function validate() {
         var p2 = /^\d{10}$/;
         var p3 = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-        if (!(p1.test(name) == true)) {
+        if (!(p1.test(name))) {
             var check1 = document.getElementById('check1');
             check1.style.visibility = 'visible';
         }
-        if (!(p2.test(phone) == true)) {
+        if (!(p2.test(phone))) {
             var check2 = document.getElementById('check2');
             check2.style.visibility = 'visible';
         }
-        if (!(p3.test(email) == true)) {
+        if (!(p3.test(email))) {
             var check3 = document.getElementById('check3');
             check3.style.visibility = 'visible';
+        }
+        if (p1.test(name) && p2.test(phone) && p3.test(email)) {
+            sendEmail()
         }
     }
 }
 
 function sendEmail() {
-    function sendEmail() {
-        // template_mhoz83a
-        var templateParams = {
-            to_name: document.getElementById("name").value,
-            to_email: document.getElementById("email").value,
-        };
-        emailjs.send('service_so58vs9', 'template_mhoz83a', templateParams)
-            .then(alert("Email sent"))
-    }
+    // template_mhoz83a
+    var templateParams = {
+        to_name: document.getElementById("name").value,
+        to_email: document.getElementById("email").value,
+    };
+
+    emailjs.send('service_so58vs9', 'template_mhoz83a', templateParams)
+        .then(alert("Email sent"))
 }
